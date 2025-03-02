3af0cb10c5d5c4de8463e9ba8f3d36dedef03e98
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2025-03-02 14:44:25 +0900

    일단 저장

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 import { skill } from '@/constant/type'
 import { ChangeEvent, useState } from 'react'
+import { characterList } from '@/constant/character'
 
 export default function Home() {
   //usestate의 미친듯한 길이...
   const [form, setForm] = useState({})
-  const [character_ID, setCharacter_ID] = useState('')
+  const [character_name, setCharacter_name] = useState('')
   const [skill_name, setSkill_name] = useState('')
   const [aka_kor, setAka_kor] = useState('')
-  const [aka_eng, setAka_eng] = useState('')
-  const [status, setStatus] = useState('basic')
-  const [command, setCommand] = useState([])
-  const [hit_position, setHit_position] = useState([])
-  const [dmg, setDmg] = useState(0)
+  const [posture, setPosture] = useState('basic')
+  const [command, setCommand] = useState<string[]>([])
+  const [hit_position, setHit_position] = useState('')
+  const [dmg, setDmg] = useState('')
   const [start, setStart] = useState('')
   const [hit, setHit] = useState('')
   const [counter, setCounter] = useState('')
@@ -26,17 +26,38 @@ export default function Home() {
     return value.split('/')
   }
 
+  const makeCommand = (value: string) => {
+    const inputCommand = value.split('')
+    const newCommand = []
+
+    for (let i = 0; i < inputCommand.length; i++) {
+      if (!isNaN(Number(inputCommand[i]))) {
+        // 숫자인 경우
+        newCommand.push(inputCommand[i])
+      } else {
+        // 문자인 경우
+        if (i + 1 < inputCommand.length) {
+          newCommand.push(inputCommand[i] + inputCommand[i + 1])
+          i++
+        } else {
+          newCommand.push(inputCommand[i])
+        }
+      }
+    }
+    setCommand(newCommand)
+  }
+
   return (
     <div className='mt-24 flex'>
       <div className='mx-auto w-[400px] flex flex-col'>
-        <div className='flex flex-row items-center gap-4 my-3  justify-between'>
-          <div className=''>캐릭터 아이디 숫자로</div>
+        <div className='flex flex-row items-center gap-4 my-3 justify-between'>
+          <div className=''>캐릭터 아이디 영어로</div>
           <input
             type='text'
             name='character_ID'
             className='py-3 border-2 pl-2 w-48 block '
-            value={character_ID}
-            onChange={(e) => setCharacter_ID(e.target.value)}
+            value={character_name}
+            onChange={(e) => setCharacter_name(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -45,22 +66,18 @@ export default function Home() {
             type='text'
             name='skill_name'
             className='py-3 border-2 pl-2 w-48 block'
+            value={skill_name}
+            onChange={(e) => setSkill_name(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
-          <div className=''>AKA 한글</div>
+          <div className=''>AKA 한글로</div>
           <input
             type='text'
             name='aka_kor'
             className='py-3 border-2 pl-2 w-48 block'
-          />
-        </div>
-        <div className='flex flex-row items-center gap-4 my-3  justify-between'>
-          <div className=''>AKA 영어</div>
-          <input
-            type='text'
-            name='aka_eng'
-            className='py-3 border-2 pl-2 w-48 block'
+            value={aka_kor}
+            onChange={(e) => setAka_kor(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -69,6 +86,8 @@ export default function Home() {
             type='text'
             name='status'
             className='py-3 border-2 pl-2 w-48 block'
+            value={posture}
+            onChange={(e) => setPosture(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -77,6 +96,8 @@ export default function Home() {
             type='text'
             name='command'
             className='py-3 border-2 pl-2 w-48 block'
+            value={command.join('')}
+            onChange={(e) => makeCommand(e.target.value)}
           />
         </div>
         <button className='p-2 bg-amber-200 cursor-pointer mt-12'>
@@ -89,7 +110,9 @@ export default function Home() {
           <input
             type='text'
             name='hit_position'
-            className='py-3 border-2 pl-2 w-48 block '
+            className='py-3 border-2 pl-2 w-48 block'
+            value={hit_position}
+            onChange={(e) => setHit_position(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -98,6 +121,8 @@ export default function Home() {
             type='text'
             name='dmg'
             className='py-3 border-2 pl-2 w-48 block'
+            value={dmg}
+            onChange={(e) => setDmg(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -106,6 +131,8 @@ export default function Home() {
             type='text'
             name='start'
             className='py-3 border-2 pl-2 w-48 block'
+            value={start}
+            onChange={(e) => setStart(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -114,6 +141,8 @@ export default function Home() {
             type='text'
             name='hit'
             className='py-3 border-2 pl-2 w-48 block'
+            value={hit}
+            onChange={(e) => setHit(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -122,6 +151,8 @@ export default function Home() {
             type='text'
             name='counter'
             className='py-3 border-2 pl-2 w-48 block'
+            value={counter}
+            onChange={(e) => setCounter(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -130,16 +161,30 @@ export default function Home() {
             type='text'
             name='guard'
             className='py-3 border-2 pl-2 w-48 block'
+            value={guard}
+            onChange={(e) => setGuard(e.target.value)}
           />
         </div>
       </div>
       <div className='mx-auto w-[400px] flex flex-col'>
+        <div className='flex flex-row items-center gap-4 my-3  justify-between'>
+          <div className=''>히트 시 아이콘</div>
+          <input
+            type='text'
+            name='description_1_kor'
+            className='py-3 border-2 pl-2 w-48 block'
+            value={description_1_kor}
+            onChange={(e) => setDescription_1_kor(e.target.value)}
+          />
+        </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
           <div className=''>한국 설명-1</div>
           <input
             type='text'
             name='description_1_kor'
             className='py-3 border-2 pl-2 w-48 block'
+            value={description_1_kor}
+            onChange={(e) => setDescription_1_kor(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -148,6 +193,8 @@ export default function Home() {
             type='text'
             name='description_2_kor'
             className='py-3 border-2 pl-2 w-48 block'
+            value={description_2_kor}
+            onChange={(e) => setDescription_2_kor(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -156,6 +203,8 @@ export default function Home() {
             type='text'
             name='description_1_eng'
             className='py-3 border-2 pl-2 w-48 block'
+            value={description_1_eng}
+            onChange={(e) => setDescription_1_eng(e.target.value)}
           />
         </div>
         <div className='flex flex-row items-center gap-4 my-3  justify-between'>
@@ -164,6 +213,8 @@ export default function Home() {
             type='text'
             name='description_2_eng'
             className='py-3 border-2 pl-2 w-48 block'
+            value={description_2_eng}
+            onChange={(e) => setDescription_2_eng(e.target.value)}
           />
         </div>
       </div>
---
+++ constant/character.ts
@@ -0,0 +1,187 @@
+export const characterList = [
+  {
+    name: 'anna',
+    dlc: true,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'alisa',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'asuka',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'azucena',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'bryan',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'claudio',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'clive',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'deviljin',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'dragunov',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'eddy',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'fengwei',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'heihachi',
+    dlc: true,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'hwoarang',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'jack',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'jin',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'jun',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'kazuya',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'king',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'kuma',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'lars',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'law',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'leo',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'lee',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'leory',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'lili',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'lidia',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'nina',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'panda',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'paul',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'raven',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'reina',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'shaheen',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'steve',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'victor',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'xiaoyu',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'yoshimitsu',
+    dlc: false,
+    posture: ['basic', 'rising'],
+  },
+  {
+    name: 'zafina',
+    dlc: false,
+    posture: ['basic', 'mantis', 'tarantula', 'rising'],
+  },
+]
